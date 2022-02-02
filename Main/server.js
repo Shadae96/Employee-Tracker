f90e85ef2bf37d1297ee44603a9907e5d973e5d3
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-02-01 23:35:51 -0500

    connect MySQL

--- Main/server.js
+++ Main/server.js
@@ -3,6 +3,33 @@ const inquirer =require("inquirer");
 const mysql = require("mysql2");
 const util = require("util");
 const cTable = require("console.table");
+const config = require ('./package.json');
+const express = require ('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database: 'employee_tracker',
+});
+
+connection.connect((err) => {
+    if (err){
+        console.log(err);
+    }
+    console.log("You are connected to MySQL");
+
+});
+
+app.listen(PORT, () => { 
+    console.log (`Server is listening on ${PORT}`);
+
+
+});
 
 
 
