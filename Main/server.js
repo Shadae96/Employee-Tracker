b6b2657d9ab5c187a77cefcb6989bb9d422d8b5c
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-02-05 03:33:51 -0500

    updated logic

--- Main/server.js
+++ Main/server.js
@@ -46,7 +46,7 @@ function startSearch() {
                 "View all employees by manager",
                 "View all employees by department",
                 "Add a new employee",
-                "Add a new departmetn",
+                "Add a new department",
                 "Add a new role",
                 "Remove an existing employee",
                 "Update employee role",
@@ -57,7 +57,7 @@ function startSearch() {
         .then( answers => {
             switch (answers.action){
                 case "View all employees":
-                    byEmployee();
+                    byEmployees();
                     startSearch();
                      break;
 
@@ -227,14 +227,166 @@ function startSearch() {
                                 })
                                      break;
 
-            
-                        }
+                    case "Update employee manager":
+                            inquirer
+                            .prompt([
+                                    {
+                                        name:"manager",
+                                        type:"input",
+                                        message:"Please enter manager id",
+                            
+                                    },
+
+                                    {
+                                        name:"Employee",
+                                        type:"input",
+                                        message:"Please enter the manager's id.",
+                            
+                                    },
+                            ])
+                                .then (answers => {
+                                    updateByManager(answers.manager, answers.Employee);
+                                    startSearch();
+                                })
+                                    break;
 
 
 
+                            }
 // End of function
 });
-        
-        
-        
         }
+
+
+// View everyone function
+function byEmployees() {
+
+    var results = connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.d_name AS department, role.salary,CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+        
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+            })
+        };
+
+// View employees by department function
+
+function byDepartment() {
+
+    var results = connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.d_name FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id WHERE department.id;",
+        
+        function (error, results) {
+            if (error) throw error
+            console.table(results)
+            })
+        };
+    
+
+// View Managers function
+        function byManager() {
+
+            var manager = connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.d_name, employee.manager_id AS department, role.title FROM employee LEFT JOIN role on role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id WHERE manager_id;",
+        
+        
+                function (error, manager) {
+                    if (error) throw error
+                    console.table(manager)
+                })
+        };
+
+//Function to update Managers
+
+function updateByManager(managerId, employeeId) {
+
+    var updateManager = connection.query(
+        "UPDATE employee SET manager_id = ? WHERE id = ?",
+        [managerId, employeeId],
+        function (error, updateManager) {
+            if (error) throw error
+        })
+
+    byManager();
+}
+
+
+// Function to add a new employee
+
+function addDepartment(employeeFirst,employeeLast, department, manager){
+
+    var add = connection.query(
+        "INSERT INTO employee SET first_name = ?, last_name = /, role_id = /, manager_id =?",
+        [employeeFirst,employeeLast, department, manager],
+        function (error, add) {
+            if (error) throw error
+        })
+        byEmployees();
+}
+
+
+// function to show departments
+function departmentTable() {
+    var depTable = connection.query ("SELECT d_name FROM department;",
+    
+    function (error, depTable) {
+        if (error) throw error
+    })
+}
+
+// Function to create a new department
+function addDepartment() {
+    var department = connection.query(
+        "INSERT INTO department SET d_name = ?",[department],
+    
+    function (error, department) {
+        if (error) throw error
+    })
+    departmentTable();
+}
+
+// Function to show role without including employees
+
+function roleTable() {
+    var roleT = connection.query(
+        "SELECT title, salary, department_id FROM role;",
+
+    function (error, roleT) {
+        if (error) throw error
+        console.table (roleT)
+    })
+}
+
+// Function to add a new role
+
+function addRole(title, salary, department_id) {
+    var newRole = connection.query(
+        "INSERT INTO role SET title = ?, salary = ?, department_id =?",
+        [title, salary,department_id],
+    function (error, newRole) {
+        if (error) throw error
+        // console.table (manager)
+    })
+    roleTable();
+}
+
+// Function to remove Employee
+
+function removeEmployee(id) {
+    var add= connection.query(
+        "DELETE FROM employee WHERE id = ?", 
+        [id],
+    function (error, id) {
+        if (error) throw error
+    })
+    byEmployees();
+}
+
+// "Update employee role"
+function updateByRole (employeeID, roleId){
+    var byRole = connect.query("UPDATE employee SET role_id = ? WHERE id = ?",
+    [roleId, employeeId],
+    function (error, role){
+        if (error) throw error
+    })
+    byDepartment();
+}
+
